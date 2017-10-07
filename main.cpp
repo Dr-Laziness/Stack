9d05b12c8a2b06708bd11464ecbe17b2c049ec40
Dr-Laziness <dr.Laziness@gmail.com>
2017-10-07 18:26:07 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
 
-using namespace std;
+#include "stack.h"
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	std::cout << "----------- START -----------" << std::endl;
+
+	Stack<char> obj_1;
+	for(int i = 0; i < 5; i++)
+	{
+		obj_1.push(97 + i);
+	}
+	std::cout << "Stack 1: " << std::endl;
+	obj_1.print();
+	std::cout << "size: " << obj_1.size() << std::endl;
+
+	Stack<char> obj_2{obj_1};
+	for(unsigned long int i = 0; i < obj_1.size(); i++)
+	{
+		obj_2.push(105 + i);
+	}
+
+	std::cout << std::endl << "Stack 2:" << std::endl;
+	obj_2.print();
+	std::cout << "size: " << obj_2.size() << std::endl;
+
+	obj_1 = obj_2;
+	std::cout << std::endl << "Stack 1:" << std::endl;
+	obj_1.print();
+	for(unsigned long int i = 0; i < obj_1.size(); i++)
+	{
+		obj_2.push(110 + i);
+	}
+	std::cout << std::endl << "Stack 2:" << std::endl;
+	obj_2.print();
+
+	obj_1 = std::move(obj_2);
+	std::cout << std::endl << "Stack 1:" << std::endl;
+	obj_1.print();
+	std::cout << "Stack 2:" << std::endl;
+	obj_2.print();
+
+	Stack<char>::Iterator it_1 = obj_1.begin();
+	std::cout << "Iterator 1: " << *it_1 << std::endl;
+
+
+	std::cout << "----------- END -----------" << std::endl;
 	return 0;
 }
--- stack.h
+++ stack.h
@@ -1,4 +1,226 @@
-#ifndef STACK_H
-#define STACK_H
+#pragma once
 
-#endif // STACK_H
+#include <iostream>
+
+template<typename T>
+class Stack
+{
+private:
+	struct Element
+	{
+		T		value;
+		Element	*next;
+	};
+
+public:
+	class Iterator
+	{
+	private:
+		Element	*_ptr_element;
+
+		friend class Stack;
+		Iterator() : _ptr_element(nullptr) {}
+		Iterator(Element *element) : _ptr_element(element) {}
+
+	public:
+		Iterator(const Iterator &it) : _ptr_element(it._ptr_element){}
+
+		Iterator operator=(const Iterator &p_iterator)
+		{
+			_ptr_element = p_iterator._ptr_element;
+			return *this;
+		}
+
+		bool operator==(const Iterator &it) const
+		{
+			return _ptr_element == it._ptr_element;
+		}
+
+		bool operator!=(const Iterator &it) const
+		{
+			return _ptr_element == it._ptr_element;
+		}
+
+		Iterator &operator++()
+		{
+			_ptr_element = _ptr_element->next;
+			return *this;
+		}
+
+		Iterator &operator++(int)
+		{
+			_ptr_element = _ptr_element->next;
+			return *this;
+		}
+
+		T &operator*() const
+		{
+			return _ptr_element->value;
+		}
+	};
+
+private:
+	unsigned long int	_size;
+	Element				*_ptr_begin;
+	Iterator			_begin;
+
+	//+++
+	Element *find_last_element()
+	{
+		Element *temp = _ptr_begin;
+		while(temp->next != nullptr)
+			temp = temp->next;
+		return temp;
+	}
+	//+++
+	void push_back(const T value)
+	{
+		Element *temp = new Element;
+		temp -> value = value;
+		temp -> next = nullptr;
+
+		if(_ptr_begin == 0)
+		{
+			_ptr_begin = temp;
+			_begin = Iterator(_ptr_begin);
+		}
+		else
+		{
+			Element *last_element = find_last_element();
+			last_element->next = temp;
+			last_element = temp;
+		}
+		_size++;
+	}
+	//+++
+	void clean_stack()
+	{
+		while (!isEmpty())
+		{
+			Element *temp = _ptr_begin;
+			_ptr_begin = _ptr_begin->next;
+			delete temp;
+		}
+		_begin = Iterator(_ptr_begin);
+		_size = 0;
+	}
+
+public:
+	//+++
+	Stack() :
+		_ptr_begin(nullptr),
+		_size(0),
+		_begin(Iterator(_ptr_begin)) { }
+	//+++
+	Stack(const Stack &p_stack) :
+		Stack()
+	{
+		Iterator temp = p_stack._begin;
+		for(unsigned long int i = 0; i < p_stack.size(); i++)
+		{
+			push_back(*temp);
+			temp++;
+		}
+	}
+
+	Stack(Stack &&p_stack) :
+		_ptr_begin(p_stack._ptr_begin),
+		_size(p_stack._size),
+		_begin(Iterator(_ptr_begin))
+	{
+		p_stack.clean_stack();
+	}
+
+	~Stack()
+	{
+		clean_stack();
+		delete _ptr_begin;
+	}
+
+	//+++
+	Stack operator=(const Stack &p_stack)
+	{
+		Iterator temp = p_stack._begin;
+		clean_stack();
+		for(unsigned long int i = 0; i < p_stack.size(); i++)
+		{
+			push_back(*temp);
+			temp++;
+		}
+		return *this;
+	}
+
+	Stack operator=(Stack &&p_stack)
+	{
+		Element *temp_ptr_beginer = p_stack._ptr_begin;
+		p_stack._ptr_begin = _ptr_begin;
+		_ptr_begin = temp_ptr_beginer;
+
+		unsigned long int temp_size = p_stack._size;
+		p_stack._size = _size;
+		_size = temp_size;
+
+		_begin = Iterator(_ptr_begin);
+		p_stack._begin = Iterator(p_stack._ptr_begin);
+
+		return *this;
+	}
+
+	//+++
+	bool isEmpty() const
+	{
+		return _ptr_begin == nullptr;
+	}
+	//+++
+	unsigned long int size() const
+	{
+		return _size;
+	}
+
+	Iterator begin()
+	{
+		return _begin;
+	}
+	//+++
+	void push(const T &value)
+	{
+		Element *temp = new Element;
+		temp->value = value;
+		temp->next = _ptr_begin;
+
+		_ptr_begin = temp;
+		_begin = Iterator(_ptr_begin);
+
+		_size++;
+	}
+	//+++
+	T pop()
+	{
+		if(isEmpty())
+		{
+			std::cout << "The stack is empty." << std::endl;
+			exit(1);
+		}
+		T value = _ptr_begin->value;
+
+		Element *temp = _ptr_begin;
+
+		_ptr_begin = _ptr_begin->next;
+		_begin = Iterator(_ptr_begin);
+		_size--;
+
+		delete temp;
+		return value;
+	}
+	//+++
+	void print()
+	{
+		Iterator temp = Iterator(_ptr_begin);
+		for(unsigned long int i = 0; i < size(); i++)
+		{
+			std::cout << *temp << " ";
+			temp++;
+		}
+		std::cout << std::endl;
+	}
+};
